4750cea00ae730bd5c4cd1b58f7804b68b0c8f7d
KanishaL <kanishaliyanage97@gmail.com>
2022-11-09 17:50:31 +0530

    home page commit

---
+++ client-side-web/jobs.php
@@ -0,0 +1,124 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Jobs</title>
+
+    <!-- main template css file -->
+    <link rel="stylesheet" href="css/template3.css">
+    <!-- normalization -->
+    <link rel="stylesheet" href="css/norm.css">
+    <!-- font awesome -->
+    <link rel="stylesheet" href="css/all.min.css">
+    <!-- googel fonts -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Cairo:wght@300;900&family=Work+Sans:wght@200;300;400;500;600;700;800&display=swap" rel="stylesheet">
+
+</head>
+
+<body>
+    <!-- start articals -->
+    <div class="articles" id="Articles">
+        <h2 class="main-titel">Articles</h2>
+        <div class="container">
+            <div class="box">
+                <img src="./imgs/cat-01.jpg">
+                <div class="content">
+                    <h3>Test Title</h3>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut exceptur</p>
+                </div>
+                <div class="info">
+                    <a href="#">Read More</a>
+                    <i class="fas fa-long-arrow-alt-right"></i>
+                </div>
+            </div>
+            <div class="box">
+                <img src="./imgs/cat-02.jpg">
+                <div class="content">
+                    <h3>Test Title</h3>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut exceptur</p>
+                </div>
+                <div class="info">
+                    <a href="#">Read More</a>
+                    <i class="fas fa-long-arrow-alt-right"></i>
+                </div>
+            </div>
+            <div class="box">
+                <img src="./imgs/cat-03.jpg">
+                <div class="content">
+                    <h3>Test Title</h3>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut exceptur</p>
+                </div>
+                <div class="info">
+                    <a href="#">Read More</a>
+                    <i class="fas fa-long-arrow-alt-right"></i>
+                </div>
+            </div>
+            <div class="box">
+                <img src="./imgs/cat-04.jpg">
+                <div class="content">
+                    <h3>Test Title</h3>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut exceptur</p>
+                </div>
+                <div class="info">
+                    <a href="#">Read More</a>
+                    <i class="fas fa-long-arrow-alt-right"></i>
+                </div>
+            </div>
+            <div class="box">
+                <img src="./imgs/cat-05.jpg">
+                <div class="content">
+                    <h3>Test Title</h3>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut exceptur</p>
+                </div>
+                <div class="info">
+                    <a href="#">Read More</a>
+                    <i class="fas fa-long-arrow-alt-right"></i>
+                </div>
+            </div>
+            <div class="box">
+                <img src="./imgs/cat-06.jpg">
+                <div class="content">
+                    <h3>Test Title</h3>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut exceptur</p>
+                </div>
+                <div class="info">
+                    <a href="#">Read More</a>
+                    <i class="fas fa-long-arrow-alt-right"></i>
+                </div>
+            </div>
+            <div class="box">
+                <img src="./imgs/cat-07.jpg">
+                <div class="content">
+                    <h3>Test Title</h3>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut exceptur</p>
+                </div>
+                <div class="info">
+                    <a href="#">Read More</a>
+                    <i class="fas fa-long-arrow-alt-right"></i>
+                </div>
+            </div>
+            <div class="box">
+                <img src="./imgs/cat-08.jpg">
+                <div class="content">
+                    <h3>Test Title</h3>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut exceptur</p>
+                </div>
+                <div class="info">
+                    <a href="#">Read More</a>
+                    <i class="fas fa-long-arrow-alt-right"></i>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="spikes"></div>
+    <!-- end articals -->
+
+</body>
+
+</html>
