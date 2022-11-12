e7433600403e58161b8154f6a9f1edcd6326b52c
KanishaL <kanishaliyanage97@gmail.com>
2022-11-13 00:37:01 +0530

    filtering

--- client-side-web/components/filter.php
+++ client-side-web/components/filter.php
@@ -1,6 +1,3 @@
-<?php session_start(); ?>
-<?php require_once('../../connection/dbconnection.php'); ?>
-
 <?php
 
 $cat = mysqli_real_escape_string($connection, $_POST['job_category']);
@@ -26,151 +23,50 @@ if ($sal == 1) {
 
 if (isset($_POST['submit'])) {
 
-    echo "Values submitted!";
-
-?>
-
-    <!DOCTYPE html>
-    <html lang="en">
-
-    <head>
-        <meta charset="UTF-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>Filters</title>
-    </head>
-
-    <body>
-
-        <p>
-
-            <?php echo $cat; ?></br>
-            <?php echo $j_loc; ?></br>
-            <?php echo $sal; ?></br>
-
-        </p>
-
-        <?php
-
-        if ($cat == "All Category" && $j_loc == "Anywhere" && $sal == "Any") {
-            $query = "SELECT * FROM jobs";
-        } elseif ($cat != "All Category" && $j_loc == "Anywhere" && $sal == "Any") {
-            $query = "SELECT * FROM jobs
+    if ($cat == "All Category" && $j_loc == "Anywhere" && $sal == "Any") {
+        $query = "SELECT * FROM jobs";
+    } elseif ($cat != "All Category" && $j_loc == "Anywhere" && $sal == "Any") {
+        $query = "SELECT * FROM jobs
             WHERE
             category = '{$cat}'";
-        } elseif ($cat == "All Category" && $j_loc != "Anywhere" && $sal == "Any") {
-            $query = "SELECT * FROM jobs
+    } elseif ($cat == "All Category" && $j_loc != "Anywhere" && $sal == "Any") {
+        $query = "SELECT * FROM jobs
             WHERE
             location = '{$j_loc}'";
-        } elseif ($cat == "All Category" && $j_loc == "Anywhere" && $sal != "Any") {
-            $query = "SELECT * FROM jobs
+    } elseif ($cat == "All Category" && $j_loc == "Anywhere" && $sal != "Any") {
+        $query = "SELECT * FROM jobs
             WHERE
             salary >= '{$sal}' AND salary < '{$sal2}'";
-        } elseif ($cat != "All Category" && $j_loc != "Anywhere" && $sal == "Any") {
-            $query = "SELECT * FROM jobs
+    } elseif ($cat != "All Category" && $j_loc != "Anywhere" && $sal == "Any") {
+        $query = "SELECT * FROM jobs
             WHERE
             category = '{$cat}'
             AND
             location = '{$j_loc}'";
-        } elseif ($cat == "All Category" && $j_loc != "Anywhere" && $sal != "Any") {
-            $query = "SELECT * FROM jobs
+    } elseif ($cat == "All Category" && $j_loc != "Anywhere" && $sal != "Any") {
+        $query = "SELECT * FROM jobs
             WHERE
             location = '{$j_loc}'
             AND
             salary >= '{$sal}' AND salary < '{$sal2}'";
-        } elseif ($cat != "All Category" && $j_loc == "Anywhere" && $sal != "Any") {
-            $query = "SELECT * FROM jobs
+    } elseif ($cat != "All Category" && $j_loc == "Anywhere" && $sal != "Any") {
+        $query = "SELECT * FROM jobs
             WHERE
             category = '{$cat}'
             AND
             salary >= '{$sal}' AND salary < '{$sal2}'";
-        } else {
-            $query = "SELECT * FROM jobs
+    } else {
+        $query = "SELECT * FROM jobs
             WHERE
             category = '{$cat}'
             AND
             location = '{$j_loc}'
             AND
             salary >= '{$sal}' AND salary < '{$sal2}'";
-        }
-
-        $result1 = mysqli_query($connection, $query);
-        ?>
-
-        <?php
-
-        if ($result1) { ?>
-
-            <?php
-
-            if (mysqli_num_rows($result1) > 0) { ?>
-
-                <?php while ($record1 = mysqli_fetch_array($result1)) {
-
-                    $_GET['j_id'] = $record1['job_id'];
-                    $com_id = $record1['company_id'];
-
-                    $query2 = "SELECT * FROM companies
-                               WHERE company_id = '{$com_id}'
-                               LIMIT 1";
-
-                    $result2 = mysqli_query($connection, $query2);
-
-                    if ($result2) {
-
-                        while ($record2 = mysqli_fetch_array($result2)) {
-
-                ?>
-                            <div class="single-job-items mb-30">
-                                <div class="job-items">
-                                    <div class="company-img">
-                                        <a href="job_details.php?job_id=<?= $_GET['j_id'] ?>"><img src="../client-side-web/assets/images/icon/job-list1.png" alt="<?php echo $record2['company_name']; ?>"></a>
-                                    </div>
-                                    <div class="job-tittle">
-                                        <a href="job_details.php?job_id=<?= $_GET['j_id'] ?>">
-                                            <h4><?php echo strtoupper($record1['job_role']) ?></h4>
-                                        </a>
-                                        <ul>
-                                            <li><?php echo $record2['company_name'] ?></li>
-                                            <li><i class="fas fa-map-marker-alt"></i><?php echo $record1['location'] ?></li>
-                                            <li>$<?php echo $record1['salary'] ?></li>
-                                        </ul>
-                                    </div>
-                                </div>
-                                <div class="items-link f-right">
-                                    <a href="job_details.php"><?php echo $record1['job_nature'] ?></a>
-                                    <span><?php echo $record1['posted_date'] ?></span>
-                                </div>
-                            </div>
-
-                        <?php
-                        }
-
-                        ?>
-
-                <?php
-
-                    }
-                } ?>
-
-        <?php }
-        } else {
-            echo "DB failed!";
-        }
-
-        ?>
-
-    </body>
-
-    </html>
-
-<?php
-
+    }
 } else {
 
     header("Location: ../find_jobs.php");
 }
 
 ?>
-
-<?php mysqli_close($connection); ?>
--- client-side-web/find_jobs.php
+++ client-side-web/find_jobs.php
@@ -3,7 +3,72 @@
 
 <?php
 
-$job_category = " ";
+if (isset($_POST['submit'])) {
+
+
+    $cat = mysqli_real_escape_string($connection, $_POST['job_category']);
+    $j_loc = mysqli_real_escape_string($connection, $_POST['job_location']);
+    $sal = mysqli_real_escape_string($connection, $_POST['salary']);
+    $sal2 = "";
+
+    if ($sal == 1) {
+        $sal = 1000;
+        $sal2 = 2000;
+    } elseif ($sal == 2) {
+        $sal = 2000;
+        $sal2 = 3000;
+    } elseif ($sal == 3) {
+        $sal = 3000;
+        $sal2 = 4000;
+    } elseif ($sal == 4) {
+        $sal = 4000;
+        $sal2 = 5000;
+    } elseif ($sal == 5) {
+        $sal = 5000;
+    }
+
+    if ($cat == "All Category" && $j_loc == "Anywhere" && $sal == "Any") {
+        $query = "SELECT * FROM jobs";
+    } elseif ($cat != "All Category" && $j_loc == "Anywhere" && $sal == "Any") {
+        $query = "SELECT * FROM jobs
+            WHERE
+            category = '{$cat}'";
+    } elseif ($cat == "All Category" && $j_loc != "Anywhere" && $sal == "Any") {
+        $query = "SELECT * FROM jobs
+            WHERE
+            location = '{$j_loc}'";
+    } elseif ($cat == "All Category" && $j_loc == "Anywhere" && $sal != "Any") {
+        $query = "SELECT * FROM jobs
+            WHERE
+            salary >= '{$sal}' AND salary < '{$sal2}'";
+    } elseif ($cat != "All Category" && $j_loc != "Anywhere" && $sal == "Any") {
+        $query = "SELECT * FROM jobs
+            WHERE
+            category = '{$cat}'
+            AND
+            location = '{$j_loc}'";
+    } elseif ($cat == "All Category" && $j_loc != "Anywhere" && $sal != "Any") {
+        $query = "SELECT * FROM jobs
+            WHERE
+            location = '{$j_loc}'
+            AND
+            salary >= '{$sal}' AND salary < '{$sal2}'";
+    } elseif ($cat != "All Category" && $j_loc == "Anywhere" && $sal != "Any") {
+        $query = "SELECT * FROM jobs
+            WHERE
+            category = '{$cat}'
+            AND
+            salary >= '{$sal}' AND salary < '{$sal2}'";
+    } else {
+        $query = "SELECT * FROM jobs
+            WHERE
+            category = '{$cat}'
+            AND
+            location = '{$j_loc}'
+            AND
+            salary >= '{$sal}' AND salary < '{$sal2}'";
+    }
+}
 
 ?>
 
@@ -85,7 +150,7 @@ $job_category = " ";
                         </div>
 
                         <!-- Job Category Listing start -->
-                        <form action="../client-side-web/components/filter.php" method="POST">
+                        <form action="../client-side-web/find_jobs.php" method="POST">
                             <div class="job-category-listing mb-50">
                                 <!-- single one -->
                                 <div class="single-listing">
@@ -172,6 +237,7 @@ $job_category = " ";
                             <div class="container">
                                 <!-- single-job-content -->
                                 <?php
+
                                 $query = "SELECT * FROM jobs";
 
                                 $result1 = mysqli_query($connection, $query);
