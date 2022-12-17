7dfa6be17a61cd93cc0027e05ae8e485f3192111
KanishaL <kanishaliyanage97@gmail.com>
2022-12-17 14:57:27 +0530

    landing page commit

--- client-side-web/components/footer.php
+++ client-side-web/components/footer.php
@@ -1,17 +1,16 @@
 <footer>
-    <!-- footer-bottom area -->
     <div class="footer-bottom-area footer-bg footer">
         <div class="container">
             <div class="footer-border">
                 <div class="footer-copy-right">
                     <p class="bottom-text">
-                        Copyright &copy;<script>
+                        HireMe | Copyright &copy;
+                        <script>
                             document.write(new Date().getFullYear());
-                        </script> All rights reserved | This website is made for CMIS 3214 Project</a>
+                        </script> All rights reserved
                     </p>
                 </div>
             </div>
         </div>
     </div>
-    <!-- Footer End-->
 </footer>
